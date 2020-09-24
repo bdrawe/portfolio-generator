ea9b01003a8a928d91dd155857fa447ecff7c9b5
B. Drawe <brycerdrawe.b@gmail.com>
2020-09-24 00:39:44 -0600

    all html will now by dynamically generated

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+const fs = require('fs');
+const generateNewPage = require('./src/page-template.js');
+
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -129,19 +133,18 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-    .then(promptProject)
-    .then(portfolioData =>{
-      console.log(portfolioData)
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generateNewPage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!')
+  })
   });
 
 
 
-// const fs = require('fs');
-// const generateNewPage = require('./src/page-template.js');
 
 
-// fs.writeFile('./index.html', generateNewPage(name,github), err => {
-//     if(err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
